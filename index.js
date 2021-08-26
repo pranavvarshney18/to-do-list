e7de3ac300fe43e775aac0dc584744cb8a191823
Pranav Varshney <pranavvarshney18@gmail.com>
2021-08-26 13:11:01 +0530

    create the directory structure, setup express server, add git, use npm start

---
+++ index.js
@@ -0,0 +1,16 @@
+//getting express module 
+const express = require("express");
+//launching express server
+const app = express();
+//defining port
+const port = 8000;
+
+
+//checking if express server is running or not
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }else{
+        console.log(`Server is up and running on port: ${port}`);
+    }
+});
