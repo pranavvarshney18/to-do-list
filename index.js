51166bea3e7ccf465c358e457595942f6b71f496
Pranav Varshney <pranavvarshney18@gmail.com>
2021-08-26 17:38:47 +0530

    add ejs module, add view, add layout, add asset for layout

--- index.js
+++ index.js
@@ -4,12 +4,27 @@ const express = require("express");
 const app = express();
 //defining port
 const port = 8000;
+//getting express ejs layout module
+const expressLayouts = require("express-ejs-layouts");
 //getting mongoose/db module
 const db = require("./config/mongoose");
 const Todo = require("./models/todo");
 
 //use express router
 app.use("/", require("./routes/index"));
+//telling express that all views must be rendered first from expressLayouts
+app.use(expressLayouts);
+//extract style and scripts from sub pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+//including static files
+app.use(express.static("./assets"));
+
+//set up view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+
 
 //checking if express server is running or not
 app.listen(port, function(err){
