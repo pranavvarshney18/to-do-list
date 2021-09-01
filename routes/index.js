151917855c76dd28291e2c154ebc5cefc3d473ec
Pranav Varshney <pranavvarshney18@gmail.com>
2021-09-01 21:48:01 +0530

    make the code responsive

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,6 @@ console.log("Router Loaded");
 //routing "/" to home_controller.ejs inside controllers folder
 router.get("/", homeController.home);
 router.post("/create-todo", homeController.createTodo);
-// router.get("/mark-for-delete/", homeController.markForDelete);
-// router.get("/delete-todo/", homeController.deleteTodo);
 router.post("/delete", homeController.delete);
 
 
